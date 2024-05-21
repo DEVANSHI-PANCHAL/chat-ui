4939d452302382f76eb0f183b2d2dae29615d2a9
Devanshi Panchal <60812678+DEVANSHI-PANCHAL@users.noreply.github.com>
2024-05-21 18:43:25 +0530

    bundle path change

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
-    publicPath: 'dist/bundle.js'
+    publicPath: '/bundle.js'
   },
   plugins: [
     new HtmlWebpackPlugin({
